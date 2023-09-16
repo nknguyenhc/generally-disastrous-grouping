6689747c784cc7983f1a56874c4f00736cc2c36b
Brennanzuz <timingout365@gmail.com>
2023-09-16 23:23:24 +0800

    Add unit selection, deselection and moving

--- GDG/game/game.gd
+++ GDG/game/game.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _unhandled_input(event):
+func _input(event):
 	if event.is_action_pressed("move_unit"):
 		move_units()
 
 func move_units():
-	for unit in $UnitSelection.selected_units.values():
-		unit.get_parent().get_node("NavigationAgent2D").target_position = get_global_mouse_position()
+	if not $UnitSelection.is_selecting:
+		for unit in $UnitSelection.selected_units:
+			unit.get_parent().get_node("NavigationAgent2D").target_position = get_global_mouse_position()
