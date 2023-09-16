7c25c96deda5cb8dcdd0910c86c29551a2192766
Brennanzuz <timingout365@gmail.com>
2023-09-16 16:09:12 +0800

    Add sheep animation blending
    Add animation blending for sheep for it to change direction based on its current direction.

--- GDG/sheep/sheep.gd
+++ GDG/sheep/sheep.gd
@@ -4,11 +4,13 @@ const SPEED = 300
 var direction
 
 func _ready():
-	$NavigationAgent2D.target_position = Vector2(50, 50)
+	pass
+#	$NavigationAgent2D.target_position = get_global_mouse_position()
 
 func _physics_process(delta):
+	$NavigationAgent2D.target_position = get_global_mouse_position()
 	direction = global_position.direction_to($NavigationAgent2D.get_next_path_position())
 	# Update animation
-	$AnimationTree.tree_root.
+	$AnimationTree.set("parameters/blend_position", direction)
 	velocity = SPEED * direction
 	move_and_slide()
