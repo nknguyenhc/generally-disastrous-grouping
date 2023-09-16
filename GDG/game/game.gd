9e5eed59ef21bf4f7357d685a79c91fd7655582a
Brennanzuz <timingout365@gmail.com>
2023-09-16 22:52:30 +0800

    Fix selected border not disappearing

--- GDG/game/game.gd
+++ GDG/game/game.gd
@@ -1,16 +1,14 @@
 extends Node2D
 
-var selected_units = []
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _input(event):
 	if event.is_action_pressed("move_unit"):
-		print("WALTER")
 		move_units()
 
 func move_units():
-	for unit in selected_units:
-		unit.get_parent().get_node("NavigationAgent2D").target_position = get_global_mouse_position()
+	if not $UnitSelection.is_selecting:
+		for unit in $UnitSelection.selected_units:
+			unit.get_parent().get_node("NavigationAgent2D").target_position = get_global_mouse_position()
