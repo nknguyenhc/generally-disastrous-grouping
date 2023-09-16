7666a02aa60f1abed0ef441c0553ce925ec402ad
Brennanzuz <timingout365@gmail.com>
2023-09-16 20:33:29 +0800

    Fix right click to move

--- GDG/game/game.gd
+++ GDG/game/game.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
+var selected_units = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _unhandled_input(event):
+func _input(event):
 	if event.is_action_pressed("move_unit"):
+		print("WALTER")
 		move_units()
 
 func move_units():
-	for unit in $UnitSelection.selected_units.values():
+	for unit in selected_units:
 		unit.get_parent().get_node("NavigationAgent2D").target_position = get_global_mouse_position()
