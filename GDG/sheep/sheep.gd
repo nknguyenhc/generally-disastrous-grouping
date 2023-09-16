1ebe01d212215e67a04dbf1402a6471526ad586c
Brennanzuz <timingout365@gmail.com>
2023-09-16 15:45:16 +0800

    Add AnimationTree
    Add AnimationTree to blend animations based on direction.
    Add NavigationAgent though doesn't work yet.

--- GDG/sheep/sheep.gd
+++ GDG/sheep/sheep.gd
@@ -1,1 +1,14 @@
-extends MovingBody
+extends CharacterBody2D
+
+const SPEED = 300
+var direction
+
+func _ready():
+	$NavigationAgent2D.target_position = Vector2(50, 50)
+
+func _physics_process(delta):
+	direction = global_position.direction_to($NavigationAgent2D.get_next_path_position())
+	# Update animation
+	$AnimationTree.tree_root.
+	velocity = SPEED * direction
+	move_and_slide()
